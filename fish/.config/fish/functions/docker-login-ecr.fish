0744283116c40c2e64610ca07637816dd1f37782
Nathan Anderson <andnat@gmail.com>
2021-08-03 18:40:50 -0400

    remove docker-login-ecr in favor of the one in our tool repo

--- fish/.config/fish/functions/docker-login-ecr.fish
+++
@@ -1,9 +0,0 @@
-function docker-login-ecr
-  set -l profile $argv[1]
-  # Gate function on aws
-  if which aws > /dev/null
-    set -l region (aws configure get region --profile $profile)
-    set -l account (aws sts get-caller-identity --profile $profile |jq -r '.Account')
-    aws ecr get-login-password --profile $profile | docker login --username AWS --password-stdin https://$account.dkr.ecr.$region.amazonaws.com
-  end
-end
